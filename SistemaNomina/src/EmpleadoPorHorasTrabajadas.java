9fb62e950a14d030cfbb638603f459ebb3a639ea
El bromas <victor010hh@gmail.com>
2026-07-31 17:31:14 -0500

    Implement EmpleadoPorHorasTrabajadas class

--- SistemaNomina/src/EmpleadoPorHorasTrabajadas.java
+++ SistemaNomina/src/EmpleadoPorHorasTrabajadas.java
@@ -7,6 +7,31 @@
  *
  * @author ALUMNO
  */
-public class EmpleadoPorHorasTrabajadas {
-    
+public class EmpleadoPorHorasTrabajadas extends Empleado {
+
+    private int horasTrabajadas;
+    private double valorHoraOrdinaria;
+
+    // Constructor
+    public EmpleadoPorHorasTrabajadas(String nombre, String cargo, int horasTrabajadas,
+                                      double valorHoraOrdinaria, HorasExtras horasExtras) {
+        super(nombre, cargo, horasTrabajadas * valorHoraOrdinaria, horasExtras);
+        this.horasTrabajadas = horasTrabajadas;
+        this.valorHoraOrdinaria = valorHoraOrdinaria;
+    }
+
+    // Getters
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public double getValorHoraOrdinaria() {
+        return valorHoraOrdinaria;
+    }
+
+    // Sobrescribe el método calcularRetencion
+    @Override
+    public double calcularRetencion() {
+        return 0;
+    }
 }
